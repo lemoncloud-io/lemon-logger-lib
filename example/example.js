d3c036ad4539d7d7a8e4c79ce135f81f3132c85f
louis <louis@lemoncloud.io>
2019-11-21 18:08:14 +0900

    chore: modify example.js

--- example/example.js
+++ example/example.js
@@ -1,10 +1,7 @@
 'use strict';
+const { Logger } = require('../lib');
 
-const { Logger, UtilsService } = require('../lib');
 const logger = new Logger('TEST');
-const logger2 = new Logger('LEMON', { showTimestamp: false });
-const logger3 = new Logger('LEMON', { showTimestamp: false, showLogType: false });
-
 logger.log('this is log', 'extra params: ', { abced: 'asdas'}, 456456);
 logger.log('this is log', 'extra params: ', { abced: 'asdas'}, 456456);
 logger.debug('this is debug = log', 'extra params: ', { abced: 'debug'}, 456534);
@@ -12,18 +9,19 @@ logger.info('this is info', 'extra params: ', { abced: 'info'}, 45645, { asdasd:
 logger.warn('this is warn', 'extra params: ', { abced: 'warn'}, 324423);
 logger.error('this is error', 'extra params: ', { abced: 'error'}, 123123124);
 
+const logger2 = new Logger('LEMON', { showTimestamp: false });
 logger2.log('this is log', 'extra params: ', { abced: 'asdas'}, 456456);
 logger2.debug('this is debug = log', 'extra params: ', { abced: 'debug'}, 456534);
 logger2.info('this is info', 'extra params: ', { abced: 'info'}, 45645, { asdasd: 'asdsd' });
 logger2.warn('this is warn', 'extra params: ', { abced: 'warn'}, 324423);
 logger2.error('this is error', 'extra params: ', { abced: 'error'}, 123123124);
 
+const logger3 = new Logger('LEMON', { showTimestamp: false, showLogType: false });
 logger3.log('this is log', 'extra params: ', { abced: 'asdas'}, 456456);
 logger3.debug('this is debug = log', 'extra params: ', { abced: 'debug'}, 456534);
 logger3.info('this is info', 'extra params: ', { abced: 'info'}, 45645, { asdasd: 'asdsd' });
 logger3.warn('this is warn', 'extra params: ', { abced: 'warn'}, 324423);
 logger3.error('this is error', 'extra params: ', { abced: 'error'}, 123123124);
-
 try {
     const _ = JSON.parse('test');
 } catch (e) {
